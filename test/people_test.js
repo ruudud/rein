f76e9999663dd84df8683bfd65bf1366ea5486b4
Pål Ruud <ruudud@gmail.com>
2012-02-02 20:58:53 +0100

    Linting

--- test/people_test.js
+++ test/people_test.js
@@ -1,15 +1,15 @@
-(function(P) {
+(function (P) {
     'use strict';
     var assert = buster.assert, refute = buster.refute;
 
     buster.testCase('PeopleModuleTest', {
         'initialization tests': {
-            'init should fill Owners Collection': function() {
+            'init should fill Owners Collection': function () {
                 P.init(true);
 
                 assert(P.Owners.length > 0);
             },
-            'backbone should update local storage': function() {
+            'backbone should update local storage': function () {
                 P.init(true);
 
                 assert(localStorage.getItem('ReindeerOwners'));
